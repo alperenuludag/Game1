ede4574391091fd5492f24d22bde4241a3fdba87
huseyin07 <hsyn_cakir199@hotmail.com>
2022-07-07 13:11:23 +0300

    asd
    asd

---
+++ RunnerGame/Assets/GameManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Slider rate_slider;
+    bool oyunsonu_Geldimi;
+    void Start()
+    {
+        PlayerPrefs.SetFloat("reward", 0f);
+        rate_slider.value = PlayerPrefs.GetFloat("reward"); 
+        bool oyunsonu_Geldimi = false;
+
+    }
+
+    public void rate_up(float odul_degeri)
+    {
+        if(PlayerPrefs.GetFloat("reward") + odul_degeri> 1)
+        {
+            PlayerPrefs.SetFloat("reward", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("reward", PlayerPrefs.GetFloat("reward") + odul_degeri);
+        }
+      
+        //Debug.Log(PlayerPrefs.GetFloat("reward"));
+        //Debug.Log(slider_Value);
+        rate_slider.value = PlayerPrefs.GetFloat("reward");
+    }
+    public void rate_down(float ceza_degeri)
+    {
+        if(PlayerPrefs.GetFloat("reward") - ceza_degeri < 0)
+        {
+            PlayerPrefs.SetFloat("reward", 0);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("reward", PlayerPrefs.GetFloat("reward") - ceza_degeri);
+        }
+        
+
+        rate_slider.value = PlayerPrefs.GetFloat("reward");
+    }
+
+    public void oyunSonu_geldimi()
+    {
+        oyunsonu_Geldimi = true;
+    }
+    void Update()
+    {
+       
+    }
+}
--- RunnerGame/Assets/Scripts/PlayerController.cs
+++ RunnerGame/Assets/Scripts/PlayerController.cs
@@ -6,90 +6,94 @@ public class PlayerController : MonoBehaviour
 {
     public bool jump = false;
     public bool slide = false;
-    public GameObject trigger;
+    bool oyunsonu_Geldimi;
+    public GameManager gamemanager;
 
-    public Animator anim;
+   Animator anim;
     void Start()
     {
-        anim = GetComponent<Animator>();
+        oyunsonu_Geldimi = false;
+           anim = GetComponent<Animator>();
     }
     private void FixedUpdate()
     {
+        if(!oyunsonu_Geldimi)
         transform.Translate(Vector3.forward * 8f * Time.deltaTime);
     }
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (!oyunsonu_Geldimi)
         {
-            if (Input.GetAxis("Mouse X") < 0)
+            if (Input.GetKey(KeyCode.Mouse0))
             {
-                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - .1f, transform.position.y,
-                    transform.position.z), .3f);
+                if (Input.GetAxis("Mouse X") < 0)
+                {
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x - .1f, transform.position.y,
+                        transform.position.z), .3f);
+                }
+                if (Input.GetAxis("Mouse X") > 0)
+                {
+                    transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + .1f, transform.position.y,
+                        transform.position.z), .3f);
+                }
             }
-            if (Input.GetAxis("Mouse X") > 0)
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + .1f, transform.position.y,
-                    transform.position.z), .3f);
+                jump = true;
             }
-        }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                jump = false;
+            }
 
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            jump = true;    
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            jump = false;
-        }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                slide = true;
+            }
+            else
+            {
+                slide = false;
+            }
 
 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            slide = true;
-        }
-        else
-        {
-            slide = false;
-        }
+            if (jump == true)
+            {
+
+                if (anim.GetBool("isJump") == false)
+                {
+                    anim.SetBool("isJump", jump);
+                    //transform.Translate(Vector3.up * 25f * Time.deltaTime);
+
+                }
 
 
-       if (jump == true)
-        {
-            
-            if(anim.GetBool("isJump") == false)
+
+            }
+            else if (jump == false)
             {
                 anim.SetBool("isJump", jump);
-                //transform.Translate(Vector3.up * 25f * Time.deltaTime);
 
             }
-                
-            
-
-        }
-        else if(jump == false)
-        {
-            anim.SetBool("isJump", jump);
-            
-        }
 
 
 
-        if (slide == true)
-        {
-            if (anim.GetBool("isSlide") == false)
+            if (slide == true)
+            {
+                if (anim.GetBool("isSlide") == false)
+                {
+                    anim.SetBool("isSlide", slide);
+                    transform.Translate(Vector3.forward * 8f * Time.deltaTime);
+                }
+            }
+            else if (slide == false)
             {
                 anim.SetBool("isSlide", slide);
-                transform.Translate(Vector3.forward * 8f * Time.deltaTime);
+
             }
         }
-        else if (slide == false)
-        {
-            anim.SetBool("isSlide", slide);
-
-        }
-
 
     }
 
@@ -103,11 +107,26 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.tag == "engel")
         {
             Debug.Log("carpti");
+            other.gameObject.SetActive(false);
+            gamemanager.rate_down(0.1f);
         }
 
         if (other.gameObject.tag == "Coin")
         {
             Destroy(other.gameObject, 0.2f);
         }
+        if (other.CompareTag("reward"))
+        {
+            Debug.Log("odul kazandik");
+            other.gameObject.SetActive(false);
+            gamemanager.rate_up(0.1f);
+        }
+        if (other.CompareTag("bolum_sonu"))
+        {
+            anim.SetBool("oyunsonu", true);// eklenecek
+            oyunsonu_Geldimi = true;
+        }
+
+
     }
 }
